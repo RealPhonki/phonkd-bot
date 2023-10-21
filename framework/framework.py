5a969b2ddc661db173972543765a44f3f7e64bf6
Phonki <138313415+RealPhonki@users.noreply.github.com>
2023-10-21 19:57:45 -0400

    reorganized and added comments

--- framework/framework.py
+++ framework/framework.py
@@ -17,30 +17,36 @@ class DiscordAPIHandler():
         self.callables = {}
 
     """ Initializer methods """
-
     def load_config(self) -> dict:
+        # check if config.json exists
         if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
             sys.exit('"config.json" file not found')
+        # open and return the contents
         else:
             with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
                 return json.load(file)
 
     def load_client(self) -> commands.Bot:
+        # create a discord bot client and return it
         client = commands.Bot(command_prefix=commands.when_mentioned_or(self.config["prefix"]), intents=discord.Intents.all())
         client.logger = logger()
         return client
 
     async def load(self) -> None:
+        # get the path of hte current file
         path = os.path.realpath(f"{os.path.dirname(__file__)}/cogs")
 
+        # if the cogs directory does not exist then cancel the script
         if not os.path.isdir(path):
             sys.exit('"cogs" directory does not exist')
 
+        # loop through every filename in the cogs directory
         for filename in os.listdir(path):
+            # only handle python files
             if filename.endswith(".py"):
                 extension = filename[:-3]
                 try:
-                    # load the python extension and print a cool message
+                    # load the cog and log the information
                     await self.client.load_extension(f"framework.cogs.{extension}")
                     self.client.logger.info(f"Loaded extension '{extension}'")
                 except Exception as e:
@@ -54,7 +60,6 @@ class DiscordAPIHandler():
             await self.client.start(self.config["token"])
 
     """ Framework methods """
-
     def call_on_message(self, function: callable) -> None:
         """
         Takes in a function that will be called whenever a message is received
@@ -62,6 +67,9 @@ class DiscordAPIHandler():
         self.callables["on_message"] = function
 
     def activate_bot(self) -> None:
+        """
+        Activates the bot
+        """
         asyncio.run(self.main())
 
 api_handler = DiscordAPIHandler()
