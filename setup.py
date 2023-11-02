5183be822a5c5abd7afe418f91eab9ba05a94483
Phonki <138313415+RealPhonki@users.noreply.github.com>
2023-11-02 05:38:49 -0400

    changed version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.11'
+VERSION = '0.0.12'
 DESCRIPTION = 'A framework that makes discord bot programming easy'
 
 with open("README.md", "r") as f:
